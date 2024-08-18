09db481547e503c73db690371e6703a68a4d47dd
keatonrproud <keatonrproud@gmail.com>
2024-08-18 06:16:59 +0200

    fix: web app

---
+++ frontend/app.py
@@ -0,0 +1,76 @@
+from random import choice
+
+from flask import Flask, Response, jsonify, redirect, render_template, request
+
+from backend.setup import create_game_and_deal
+from backend.turn import computer_choice_logic
+from briscola.client import BriscolaGame
+from card_game.table.table_settings import Direction
+from play_web.web_logic import web_play_card
+
+app = Flask(__name__)
+game: BriscolaGame = create_game_and_deal(
+    player_count=2, play_direction=Direction.COUNTER_CLOCKWISE, computer_count=1
+)
+
+
+@app.route("/")
+def index() -> str:
+    # reset the game and deck
+    game.reset_game()
+    return render_template("index.html")
+
+
+@app.route("/turn")
+def turn() -> str:
+    return render_template("turn.html")
+
+
+@app.route("/api/get_state", methods=["GET"])
+def get_state() -> Response:
+    return jsonify(game.to_dict())
+
+
+# TODO some issue where I play a card, then it doen't get taken as the computer plays it?
+
+# TODO how to visualize computer's plays?
+#  see part of face down cards sticking out from top of screen, and when I play it goes to their turn and shows what I play, then their selection creates an animation + flip
+
+
+@app.route("/api/play_card", methods=["POST"])
+def play_card() -> Response:
+    res = request.json
+    if res is None:
+        return Response(status=400)
+
+    if card_idx := res.get("card_index") is None:
+        return Response(status=400)  # Bad request if card_index is not provided
+
+    web_play_card(card_idx=card_idx, game=game)
+
+    while not game.active_player.is_person:
+        random_choice_idx = computer_choice_logic(game=game, cards=game.active_player.hand.cards)
+        web_play_card(card_idx=random_choice_idx, game=game)
+
+    return jsonify(game.to_dict())  # Return updated game state
+
+
+@app.route("/end_game")
+def end_game() -> str:
+    max_score = max(player.score for player in game.players)
+    winner = next((player for player in game.players if player.score > game.win_condition), None)
+
+    if winner:
+        winner_message = f"{winner} wins with {winner.score} points!"
+    else:
+        tied_players = [str(player) for player in game.players if player.score == max_score]
+        if len(tied_players) > 1:
+            winner_message = f"The game ends in a tie with {' and '.join(tied_players)} having {max_score} points!"
+        else:
+            winner_message = f"{tied_players[0]} wins with {max_score} points!"
+
+    return render_template("end_game.html", winner_message=winner_message)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
